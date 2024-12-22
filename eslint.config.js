ebc859086cf80ffcabcce4c251ad305d2bebf20c
yamata <codeindevelopment@gmail.com>
2024-12-23 02:53:33 +0330

    implement eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {ignores: ['dist']},
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      // react
+      react.configs.flat.recommended,
+      // import
+      importPlugin.flatConfigs.recommended,
+      // a11y (accessibility
+      jsxA11y.flatConfigs.recommended,
+      // prettier
+      prettier,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -17,12 +32,23 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    settings: {
+      // for eslint-plugin-react to auto detect react version
+      react: {
+        version: 'detect',
+      },
+      // for eslint-plugin-import to use import alias
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react/button-has-type': 'error',
+      'react/react-in-jsx-scope': ['off'],
+      'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
     },
-  },
-)
+  }
+);
