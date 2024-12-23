35c77464584ca68e18aa5668d811b36cde81a1e6
yamata <codeindevelopment@gmail.com>
2024-12-23 06:12:20 +0330

    feat: integrate Tailwind CSS and refactor App component layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 
+import '@/assets/css/tailwind.css';
 import '@/assets/scss/style.scss';
 
 import App from './App.tsx';
